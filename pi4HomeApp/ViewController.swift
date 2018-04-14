c165778454933013973966c3f6cf7d1a45fcf08d
Josef Grinspun <josef.grinspun@bitingbit.com>
2018-04-14 15:00:27 +0200

    Initial Commit

---
+++ pi4HomeApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pi4HomeApp
+//
+//  Created by Josef Grinspun on 14.04.18.
+//  Copyright © 2018 Biting Bit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
